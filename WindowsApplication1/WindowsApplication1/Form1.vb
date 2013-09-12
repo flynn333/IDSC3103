042550b192d6a35290a90d0f47bf746f6184e8ea
flynn333 <flynn333@umn.edu>
2013-09-11 20:48:51 -0500

    added app1 aka HW1

---
+++ WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class FrmHome
+
+
+
+    Private Sub MoveExit_Click(sender As System.Object, e As System.EventArgs) Handles MoveExit.Click, MoveExit.MouseEnter
+
+        Dim NumX As New Integer
+        Dim NumY As New Integer
+        NumX = Rnd() * CInt(Me.Width)
+        NumX = Math.Round(NumX)
+        NumY = Rnd() * CInt(Me.Height)
+        NumY = Math.Round(NumY)
+        MoveExit.Location = New Point(NumX, NumY)
+
+    End Sub
+
+End Class
